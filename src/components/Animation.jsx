13bb8716fad58cd7c3302841fac2d1dc0b6503d6
Thuong Vo Hoai <hoaithuong.data@gmail.com>
2024-03-01 20:22:59 +0700

    add more skill

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -1,6 +1,19 @@
 import { TypeAnimation } from "react-type-animation";
+import React, { useState, useEffect } from "react";
 
 export default Animation = () => {
+  const [pageURL, setPageURL] = useState("dev");
+  const [type, setType] = useState("Software");
+
+  useEffect(() => {
+    setPageURL(window.location.href);
+  })
+  console.log(pageURL);
+  if(pageURL.includes("devops")) {
+    setType("DevOps");
+  }
+
+
   return (
     <TypeAnimation
       sequence={[
@@ -8,9 +21,9 @@ export default Animation = () => {
         500, // Waits 1s
         "I'm a", // Deletes 'One' and types 'Two'
         1000, // Waits 2s
-        "I'm a Software ", // Types 'Three' without deleting 'Two'
+        "I'm a " + type  + " ", // Types 'Three' without deleting 'Two'
         1000,
-        "I'm a Software Engineer.", // Types 'Three' without deleting 'Two'
+        "I'm a "+ type +" Engineer.", // Types 'Three' without deleting 'Two'
         2000
       ]}
       wrapper="div"
